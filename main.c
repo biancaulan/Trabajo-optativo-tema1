7285e763d6ba3786ebc246ffc5332547379df27d
biancaulan <biancaulan12@gmail.com>
2024-03-05 19:43:40 +0100

    Fin

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 //Funcion que devuelve el numero de bits de n
 int numero_de_bits(int nodos_total){
     int numero = nodos_total;
@@ -31,7 +38,7 @@ void imprimir_binario(int array[], int tamano){
         printf("%i ", array[i]);
     }
 }
-//NECESARIO?????????????
+
 int binario_a_entero(int array[], int tamano){
     int resultado = 0;
     int i;
@@ -44,17 +51,27 @@ int binario_a_entero(int array[], int tamano){
     return resultado;
 }
 //Función que devuelve el XOR de dos arrays y su distancia
-void funcion_XOR_y_distancia(int array1[], int array2[], int array3[], int tamano, int *distancia){
+void funcion_XOR(int array1[], int array2[], int array3[], int tamano){
 
     for(int i=0; i< tamano; i++){
         if(array1[i]!= array2[i]){
             array3[i] = 1;
-            (*distancia)++;
         }else{
             array3[i] = 0;
         }
     }
 }
+
+int calc_distancia_XOR(int array1[], int array2[], int tamano){
+    int distancia = 0;
+    for(int i=0; i< tamano; i++){
+        if(array1[i]!= array2[i]){
+            distancia++;
+        }
+    }
+    return distancia;
+}
+
 //Calcula (x,y) del num
 void calc_puntos(int num, int n, int k, int puntos[]){
     int aux_num = num;
@@ -80,8 +97,9 @@ void calc_puntos(int num, int n, int k, int puntos[]){
     printf(" ] ");
 
 }
-int calc_distancia(int reg_enc[], int distancia, int n){
+int calc_distancia(int reg_enc[], int n){
     int i;
+    int distancia = 0;
     for(i=0; i<n; i++){
         distancia+= abs(reg_enc[i]);
     }
@@ -89,17 +107,23 @@ int calc_distancia(int reg_enc[], int distancia, int n){
 }
 void calc_reg_enc(int arr_origen[], int arr_destino[], int n, int reg_enc[]){
     int i;
-    printf("\n");
-    printf("Registro de encaminamiento: [");
     for(i=0; i<n; i++){
         reg_enc[i] = arr_destino[i]-arr_origen[i];
-        if(i == n-1){
-            printf("%i ", reg_enc[i]);
+    }
+
+}
+void transformar(int reg_enc[], int k, int transformado[], int n){
+    int i;
+    int medio = k/2;
+    for(i=0; i<n; i++){
+        if(reg_enc[i] > medio){
+            transformado[i] = reg_enc[i]-k;
+        }else if(reg_enc[i]< -medio){
+            transformado[i] = reg_enc[i]+k;
         }else{
-            printf("%i, ", reg_enc[i]);
+            transformado[i] = reg_enc[i];
         }
     }
-    printf(" ]\n");
 
 }
 
@@ -137,21 +161,80 @@ void registro_enc_XOR(int array_origen[], int XOR[], int reg_enc[], int tamano, 
         }
     }
 }
+int coordenadas_a_num(int reg_enc[], int n, int k){
+    int num=0;
+    int i;
+    for(i=n-1; i>=0; i--){
+        num += reg_enc[i]*(pow(k, n-i-1));
+    }
+    return num;
+}
+
+void camino_malla(int puntos_origen[], int origen, int destino, int reg_enc[], bool aux, int k, int n, int distancia){
+    int i,num;
+    if(aux == false){ //malla
+        for(i=n-1; i>=0; i--){
+            while(reg_enc[i]!=0){
+                 if(reg_enc[i]<0){
+                    num = coordenadas_a_num(puntos_origen, n, k);
+                    printf("%i ->", num);
+                    reg_enc[i] = reg_enc[i] + 1;
+                    puntos_origen[i] = puntos_origen[i]-1;
+
+                 }else{
+                    num = coordenadas_a_num(puntos_origen, n, k);
+                    printf("%i->", num);
+                    reg_enc[i] = reg_enc[i] - 1;
+                    puntos_origen[i] = puntos_origen[i]+1;
+
+                 }
+            }
+            printf("%i", destino);
+        }
+
+    }else{//toro
+        for(i=n-1; i>=0; i--){
+             while(reg_enc[i]!=0){
+                    if(reg_enc[i]<0){//negativo
+                        num = coordenadas_a_num(puntos_origen, n, k);
+                        printf("%i ->", num);
+                        if(puntos_origen[i]-1 <0){
+                            puntos_origen[i] = k-1;
+                        }else{
+                            puntos_origen[i] = puntos_origen[i]-1;
+                        }
+                        reg_enc[i] = reg_enc[i] + 1;
+                    }else{//positivo
+                        num = coordenadas_a_num(puntos_origen, n, k);
+                        printf("%i ->", num);
+                        if(puntos_origen[i]+1 == k){
+                            puntos_origen[i] = 0;
+                        }else{
+                            puntos_origen[i] = puntos_origen[i]+1;
+                        }
+                        reg_enc[i] = reg_enc[i] - 1;
+
+                    }
+                }
+                printf("%i ", destino);
+
+            }
+    }
+}
 
 
 int main(int argc, char* argv[]){
 
     const char* topologia_str[] = {"toro", "hipercubo", "malla"};
-    int dimensiones, nodos, anillos, origen, destino, nodos_total, bits, distancia, d;
+    int dimensiones, nodos, anillos, origen, destino, nodos_total, bits, distancia;
     bool a_anillos = false;
     int i;
 
-
-    printf("Introduzca numero de dimensiones:\n");
+    printf("Introduzca numero de dimensiones: ");
     scanf("%i", &dimensiones);
-    printf("Introduzca el número de nodos por dimension\n");
+    printf("Introduzca el número de nodos por dimension: ");
     scanf("%i", &nodos);
-    printf("Tiene anillos? 0=no 1=si\n");
+    printf("Tiene anillos? 0=no 1=si: ");
     scanf("%i", &anillos);
 
     if(anillos == 0){
@@ -162,13 +245,13 @@ int main(int argc, char* argv[]){
         printf("Tiene que introducir un valor entre 0 y 1\n");
         return 1;
     }
-    printf("Introduzca un nodo origen\n");
+    printf("Introduzca un nodo origen: ");
     scanf("%i", &origen);
-    printf("Introduzca un nodo destino\n");
+    printf("Introduzca un nodo destino: ");
     scanf("%i", &destino);
 
     if(a_anillos == true){ //toro
-
+        printf("\n");
         nodos_total = pow(nodos, dimensiones);
         printf("%i-ary %i-%s: %i nodos en total\n", nodos, dimensiones, topologia_str[0], nodos_total);
 
@@ -178,6 +261,24 @@ int main(int argc, char* argv[]){
         calc_puntos(origen, dimensiones, nodos, puntos1);
         printf("a %i", destino);
         calc_puntos(destino, dimensiones, nodos, puntos2);
+        int reg_enc[dimensiones];
+        int transformado[dimensiones];
+        printf("\n");
+        calc_reg_enc(puntos1, puntos2, dimensiones, reg_enc);
+        transformar(reg_enc, nodos, transformado, dimensiones);
+        printf("Registro de encaminamiento: [ ");
+        for(i=0; i< dimensiones; i++){
+            if(i == dimensiones-1){
+                printf("%i ]\n", transformado[i]);
+            }else{
+                printf("%i, ", transformado[i]);
+            }
+
+        }
+        distancia = calc_distancia(transformado, dimensiones);
+        printf("Distancia: %i\n", distancia);
+        camino_malla(puntos1, origen, destino, transformado, a_anillos, nodos, dimensiones, distancia);
+
 
     }else if(a_anillos == false && nodos == 2 ){ //hipercubo
         nodos_total = pow(2, dimensiones);
@@ -202,13 +303,13 @@ int main(int argc, char* argv[]){
             printf("%i ", miArray2[i]);
         }
         printf("]\n");
-        funcion_XOR_y_distancia(miArray1, miArray2, miarray3, bits, &distancia);
+        funcion_XOR(miArray1, miArray2, miarray3, bits);
+        distancia = calc_distancia_XOR(miArray1, miArray2, bits);
         printf("Distancia : %i\n", distancia);
 
 
         printf("Registro de encaminamiento: ");
         printf("[ ");
-
 
         for(i = 0; i<bits; i++){
             printf("%i ", miarray3[i]);
@@ -229,11 +330,19 @@ int main(int argc, char* argv[]){
         calc_puntos(destino, dimensiones, nodos, puntos2);
         int reg_enc[dimensiones];
         calc_reg_enc(puntos1, puntos2, dimensiones, reg_enc);
-        distancia = calc_distancia(reg_enc, d, dimensiones);
-        printf("Distancia: %i ", distancia);
+        printf("\n");
+        printf("Registro de encaminamiento: [");
+        for(i=0; i<dimensiones; i++)
+            if(i == dimensiones-1){
+                printf("%i ", reg_enc[i]);
+            }else{
+                printf("%i, ", reg_enc[i]);
+            }
 
-
+        printf(" ]\n");
+        distancia = calc_distancia(reg_enc, dimensiones);
+        printf("Distancia: %i\n ", distancia);
+        camino_malla(puntos1, origen, destino,reg_enc, a_anillos, nodos, dimensiones, distancia);
     }
-
     return 0;
 }
